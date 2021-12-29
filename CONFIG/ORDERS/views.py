2ae95146f8d230d497a0241b0164f5b82f51bdf6
Berkay Şen <brktrl@protonmail.ch>
2021-12-29 21:34:53 +0300

    Update

--- CONFIG/ORDERS/views.py
+++ CONFIG/ORDERS/views.py
@@ -6,11 +6,11 @@ from ORDERS.models import OrderItemModel,OrderModel
 def v_payment(request):
     orderItems = CartModel.objects.filter(customer_id=request.user.id)
     ORDER      = OrderModel.objects.create(customer_id=request.user.id,complete=False,orderAddress_id=4)
-    #ORDER.save()
+    ORDER.save()
     for item in orderItems:
         print(item.product.id)
         ITEM=OrderItemModel.objects.create(product_id=item.product.id,quantity=item.amount,order_id=ORDER.id)
-        #ITEM.save()
-    #orderItems.delete()
+        ITEM.save()
+    orderItems.delete()
 
-    return render(request,"cart.html")
+    return render(request,"success.html")
