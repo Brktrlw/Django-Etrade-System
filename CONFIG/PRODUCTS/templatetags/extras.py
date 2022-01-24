c214b9693317492b4ee2664b5f58a62519c0d2ac
Berkay Şen <brktrl@protonmail.ch>
2022-01-24 17:11:37 +0300

    Update

--- CONFIG/PRODUCTS/templatetags/extras.py
+++ CONFIG/PRODUCTS/templatetags/extras.py
@@ -1,4 +1,7 @@
+import requests
 from django import template
+from PRODUCTS.models import ProductCategorieModel
+from USERAPP.models import CartModel
 register = template.Library()
 
 @register.filter()
@@ -9,4 +12,14 @@ def is_numeric (value):
     except:
         return False
 
+@register.simple_tag
+def f_cat_list():
+    categories=ProductCategorieModel.objects.all()
+    return categories
+
+
+@register.simple_tag
+def f_countOfBasket(request):
+    total=CartModel.objects.filter(customer_id=request.user.id).count()
+    return total
 
